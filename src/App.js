89da3b61522acba28f7e032c11be11b9c6597c87
Amir Mursal <amir.mursal@communitybrands.com>
2020-03-17 16:40:25 +0530

    replace browserrouter with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Layout from "../src/components/layout/Layout";
-import { Router, Route, Switch } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import { createHashHistory } from "history";
 import Login from "../src/components/login/Login";
 import UserProvider from "../src/components/provider/UserProvider";
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 
 const LayoutComponent = props => <Layout {...props} />;
 const LoginComponent = props => <Login {...props} />;
@@ -14,12 +14,12 @@ export default class App extends React.Component {
   render() {
     return (
       <UserProvider>
-        <Router history={history}>
+        <HashRouter history={history}>
           <Switch>
             <Route path="/login" component={LoginComponent} />
             <Route path="/" component={LayoutComponent} />
           </Switch>
-        </Router>
+        </HashRouter>
       </UserProvider>
     );
   }

345178a7d1ecd721bfad35cf97f152c90a19fb04
Amir Mursal <amir.mursal@communitybrands.com>
2020-06-10 14:07:52 +0530

    updated prod url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const serverUrl = "ptosystem.aptify.com"; //"dsweb-test5a.aptify.com"; //"app.aptify.com"; // ptosystem.aptify.com
+export const serverUrl = "app.aptify.com"; //"dsweb-test5a.aptify.com"; //"app.aptify.com"; // ptosystem.aptify.com
